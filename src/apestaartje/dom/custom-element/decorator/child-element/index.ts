9701c36cb6afa08a265cbfa700c36455729f46ee
Joppe <joppe@apestaartje.info>
2019-09-27 16:46:15 +0200

    Added index files

---
+++ src/apestaartje/dom/custom-element/decorator/child-element/index.ts
@@ -0,0 +1,2 @@
+export * from './ChildElement';
+export * from './ChildElements';
---
+++ src/apestaartje/dom/custom-element/decorator/input/index.ts
@@ -0,0 +1,4 @@
+export * from './castValue';
+export * from './Input';
+export * from './InputConfig';
+export * from './InputType';
