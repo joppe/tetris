888f9892b2ecc8ee2cf3fd9ba62a11f95b02f566
Joppe <joppe@apestaartje.info>
2019-10-09 20:44:23 +0200

    Added SafeObserver unit tests

--- test/unit/apestaartje/observable/observer/SafeObserver.spec.ts
+++ test/unit/apestaartje/observable/observer/SafeObserver.spec.ts
@@ -1,10 +1,10 @@
-import { Observer } from '@apestaartje/observable/observer/Observer';
 import { SafeObserver } from '@apestaartje/observable/observer/SafeObserver';
 import { Subscription } from '@apestaartje/observable/observable/Subscription';
 
 type NextableObserver<T> = { next(v: T): void };
 type ErrorableObserver<T> = { error(e: Error): void };
 type CompletableObserver<T> = { complete(): void };
+type CompleteObserver<T> = NextableObserver<T> & ErrorableObserver<T> & CompletableObserver<T>;
 
 describe('SafeObserver', (): void => {
     describe('next', (): void => {
@@ -105,6 +105,116 @@ describe('SafeObserver', (): void => {
             }).toThrowError('??');
 
             expect(unsubscriptionSpy).toHaveBeenCalled();
+        });
+    });
+
+    describe('complete', (): void => {
+        it('if the observer has no complete method, do nothing', (): void => {
+            const s: SafeObserver<number> = new SafeObserver<number>({});
+
+            s.complete();
+        });
+
+        it('if complete method is defined, call it with', (): void => {
+            const o: CompletableObserver<number> = {
+                complete(): void {
+                    // nothing
+                },
+            };
+            const u: Subscription = {
+                unsubscribe(): void {
+                    // nothing
+                },
+            };
+            const unsubscriptionSpy: jasmine.Spy = spyOn(u, 'unsubscribe');
+            const s: SafeObserver<number> = new SafeObserver<number>(o);
+            s.registerSubscription(u);
+
+            const completeSpy: jasmine.Spy = spyOn(o, 'complete');
+
+            s.complete();
+
+            expect(completeSpy).toHaveBeenCalled();
+            expect(unsubscriptionSpy).toHaveBeenCalled();
+        });
+
+        it('when an exception is thrown the observer will unsubscribe and throw an error', (): void => {
+            const e: Error = new Error('??');
+            const o: CompletableObserver<number> = {
+                complete(): void {
+                    throw e;
+                },
+            };
+            const u: Subscription = {
+                unsubscribe(): void {
+                    // nothing
+                },
+            };
+            const s: SafeObserver<number> = new SafeObserver<number>(o);
+            s.registerSubscription(u);
+
+            const unsubscriptionSpy: jasmine.Spy = spyOn(u, 'unsubscribe');
+
+            expect((): void => {
+                s.complete();
+            }).toThrowError('??');
+
+            expect(unsubscriptionSpy).toHaveBeenCalled();
+        });
+    });
+
+    describe('unsubscribe', (): void => {
+        it('when called and a subscription is registered, unsubscribe it', (): void => {
+            const s: SafeObserver<number> = new SafeObserver<number>({});
+            const u: Subscription = {
+                unsubscribe(): void {
+                    // nothing
+                },
+            };
+            s.registerSubscription(u);
+
+            const unsubscriptionSpy: jasmine.Spy = spyOn(u, 'unsubscribe');
+
+            s.unsubscribe();
+
+            expect(unsubscriptionSpy).toHaveBeenCalled();
+        });
+
+        it('once unsubscribed, next/complete/error won\'t be called anymore', (): void => {
+            const o: CompleteObserver<number> = {
+                next(v: number): void {
+                    // nothing
+                },
+                error(e: Error): void {
+                    // nothing
+                },
+                complete(): void {
+                    // nothing
+                },
+            };
+            const s: SafeObserver<number> = new SafeObserver<number>(o);
+            const u: Subscription = {
+                unsubscribe(): void {
+                    // nothing
+                },
+            };
+            s.registerSubscription(u);
+
+            const unsubscriptionSpy: jasmine.Spy = spyOn(u, 'unsubscribe');
+            const nextSpy: jasmine.Spy = spyOn(o, 'next');
+            const errorSpy: jasmine.Spy = spyOn(o, 'error');
+            const completeSpy: jasmine.Spy = spyOn(o, 'complete');
+
+            s.unsubscribe();
+
+            s.next(12);
+            s.error(new Error('??'));
+            s.complete();
+
+            expect(unsubscriptionSpy).toHaveBeenCalled();
+            expect(nextSpy).not.toHaveBeenCalled();
+            expect(errorSpy).not.toHaveBeenCalled();
+            expect(completeSpy).not.toHaveBeenCalled();
         });
     });
 });
