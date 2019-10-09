77bb1f61ae1fdd2d0b9ae5edcf3c7259d365b17c
Joppe <joppe@apestaartje.info>
2019-10-09 20:44:00 +0200

    Added filter operator

--- src/apestaartje/observable/operator/index.ts
+++ src/apestaartje/observable/operator/index.ts
@@ -1,7 +1,9 @@
+import * as filter from './filter';
 import * as map from './map';
 
 export * from './Operator';
 
 export {
+    filter,
     map,
 };
---
+++ test/unit/apestaartje/observable/operator/filter/filter.spec.ts
@@ -0,0 +1,73 @@
+import { filter } from '@apestaartje/observable/operator/filter/filter';
+import { Operator } from '@apestaartje/observable/operator/Operator';
+import { Observable } from '@apestaartje/observable/observable/Observable';
+import { Subscription } from '@apestaartje/observable/observable/Subscription';
+import { Subscriber } from '@apestaartje/observable/observable/Subscriber';
+import { SafeObserver } from '@apestaartje/observable/observer/SafeObserver';
+
+type NextableObserver<T> = { next(v: T): void };
+type ErrorableObserver<T> = { error(e: Error): void };
+type CompletableObserver<T> = { complete(): void };
+type CompleteObserver<T> = NextableObserver<T> & ErrorableObserver<T> & CompletableObserver<T>;
+
+describe('filter', (): void => {
+    let next: (v: number) => void;
+    let complete: () => void;
+    let error: (err: Error) => void;
+    let observable: Observable<number>;
+    let subscriber: Subscriber<number>;
+
+    beforeEach((): void => {
+        subscriber = (o: SafeObserver<number>): Subscription => {
+            next = (v: number): void => {
+                o.next(v);
+            };
+
+            complete = (): void => {
+                o.complete();
+            };
+
+            error = (err: Error): void => {
+                o.error(err);
+            };
+
+            return {
+                unsubscribe(): void {
+                    // nothing
+                },
+            };
+        };
+
+        observable = new Observable<number>(subscriber);
+    });
+
+    it('only emits values that are not filtered out', (): void => {
+        const f: Operator<number, number> = filter<number>((value: number): boolean => {
+            return value % 2 === 0;
+        });
+        const o: CompleteObserver<number> = {
+            next(v: number): void {
+                // nothing
+            },
+            error(e: Error): void {
+                // nothing
+            },
+            complete(): void {
+                // nothing
+            },
+        };
+        const filtered: Observable<number> = f(observable);
+        const nextSpy: jasmine.Spy = spyOn(o, 'next');
+
+        filtered.subscribe(o);
+
+        next(0);
+        expect(nextSpy).toHaveBeenCalledWith(0);
+        nextSpy.calls.reset();
+        next(1);
+        expect(nextSpy).not.toHaveBeenCalled();
+        nextSpy.calls.reset();
+        next(16);
+        expect(nextSpy).toHaveBeenCalledWith(16);
+    });
+});
