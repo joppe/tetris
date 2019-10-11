644c15b13b1d40aeaaba1af42cba2dbb0f8b5a51
Joppe <joppe@apestaartje.info>
2019-10-11 11:18:37 +0200

    Added extra unit tests

--- test/unit/apestaartje/observable/observable/element/fromElement.spec.ts
+++ test/unit/apestaartje/observable/observable/element/fromElement.spec.ts
@@ -1,5 +1,6 @@
 import { fromElement } from '@apestaartje/observable/observable/element/fromElement';
 import { Observable } from '@apestaartje/observable/observable/Observable';
+import {Subscription} from '@apestaartje/observable/observable';
 
 type NextableObserver<T> = { next(v: T): void };
 type ErrorableObserver<T> = { error(e: Error): void };
@@ -33,5 +34,16 @@ describe('fromElement', (): void => {
         el.click();
 
         expect(nextSpy).toHaveBeenCalled();
+    });
+
+    it('when unsubscribed, the event listener will be removed', (): void => {
+        const observable: Observable<Event> = fromElement(el, 'click');
+        const removeEventListenerSpy: jasmine.Spy = spyOn(el, 'removeEventListener');
+        const subscription: Subscription = observable.subscribe(observer);
+
+        el.click();
+        subscription.unsubscribe();
+
+        expect(removeEventListenerSpy).toHaveBeenCalled();
     });
 });
--- test/unit/apestaartje/observable/subject/Subject.spec.ts
+++ test/unit/apestaartje/observable/subject/Subject.spec.ts
@@ -2,30 +2,15 @@ import { Subject } from '@apestaartje/observable/subject/Subject';
 import { Subscription } from '@apestaartje/observable/observable/Subscription';
 
 type NextableObserver<T> = { next(v: T): void };
-type ErrorableObserver<T> = { error(e: Error): void };
-type CompletableObserver<T> = { complete(): void };
-type CompleteObserver<T> = NextableObserver<T> & ErrorableObserver<T> & CompletableObserver<T>;
 
 describe('Subject', (): void => {
-    const observer1: CompleteObserver<number> = {
+    const observer1: NextableObserver<number> = {
         next(v: number): void {
-            // nothing
-        },
-        error(e: Error): void {
-            // nothing
-        },
-        complete(): void {
             // nothing
         },
     };
-    const observer2: CompleteObserver<number> = {
+    const observer2: NextableObserver<number> = {
         next(v: number): void {
-            // nothing
-        },
-        error(e: Error): void {
-            // nothing
-        },
-        complete(): void {
             // nothing
         },
     };
@@ -52,6 +37,19 @@ describe('Subject', (): void => {
         subject.next(33);
         subscription1.unsubscribe();
         subscription2.unsubscribe();
+
+        expect(completeSpy).toHaveBeenCalled();
+    });
+
+    it('when unsubscribe is called on subject all observables are unsubscribed', (): void => {
+        const subject: Subject<number> = new Subject();
+        const completeSpy: jasmine.Spy = spyOn(subject, 'complete');
+
+        subject.subscribe(observer1);
+        subject.subscribe(observer2);
+
+        subject.next(33);
+        subject.unsubscribe();
 
         expect(completeSpy).toHaveBeenCalled();
     });
